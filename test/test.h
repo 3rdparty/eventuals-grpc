8fc2a02e176b374e0808a6dd8c1668c273b6044a
Benjamin Hindman <benjamin.hindman@gmail.com>
2022-02-20 10:45:06 -1000

    Use 'std::to_string()' instead of 'stringify()'

--- test/test.h
+++ test/test.h
@@ -35,17 +35,3 @@ class EventualsGrpcTest : public ::testing::Test {
 std::filesystem::path GetRunfilePathFor(const std::filesystem::path& runfile);
 
 ////////////////////////////////////////////////////////////////////////
-
-// TODO(benh): Move to stout-stringify.
-template <typename T>
-std::string stringify(const T& t) {
-  std::ostringstream out;
-  out << t;
-  if (!out.good()) {
-    std::cerr << "Failed to stringify!" << std::endl;
-    abort();
-  }
-  return out.str();
-}
-
-////////////////////////////////////////////////////////////////////////
